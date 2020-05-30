10d369d642a3f6f6f005ced53d0e0057fc7ec48b
jooyena <jooyena03@gmail.com>
2020-05-30 13:37:37 +0900

    notification

--- app/src/main/java/kr/ac/jbnu/se/mobile/ganogano/AddNoteActivity.java
+++ app/src/main/java/kr/ac/jbnu/se/mobile/ganogano/AddNoteActivity.java
@@ -1,8 +1,15 @@
 package kr.ac.jbnu.se.mobile.ganogano;
 
 import android.app.DatePickerDialog;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +18,7 @@ import android.widget.DatePicker;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -33,6 +41,7 @@ public class AddNoteActivity extends AppCompatActivity {
     private TextView resultTxt;
     private Button saveButton;
     final String dayformat = "%d 년 %d 월 %d일"; //날자형식
+    public static final String NOTIFICATION_CHANNEL_ID = "10001";
 
     // DatePicker 에서 날짜 선택 시 호출
     private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
@@ -43,6 +52,7 @@ public class AddNoteActivity extends AppCompatActivity {
             resultTxt.setText(String.format(dayformat, a_year, a_monthOfYear, a_dayOfMonth));
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,7 +77,8 @@ public class AddNoteActivity extends AppCompatActivity {
                 final int month = mCalendar.get(Calendar.MONTH);
                 final int day = mCalendar.get(Calendar.DAY_OF_MONTH);
 
-                DatePickerDialog dialog = new DatePickerDialog(AddNoteActivity.this, mDateSetListener, year, month, day);
+                DatePickerDialog dialog = new DatePickerDialog(AddNoteActivity
+                        .this, mDateSetListener, year, month, day);
                 dialog.show();
             }
         });
@@ -89,11 +100,12 @@ public class AddNoteActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //TODO : SAVE시 DB 저장
+                //TODO : activity 변경
+                Do_Notification();
                 //finish()
             }
         });
     }
-
     /**
      * D-day 반환
      */
@@ -120,6 +132,43 @@ public class AddNoteActivity extends AppCompatActivity {
 
         final String strCount = (String.format(strFormat, result));
         return strCount; //결과 값 반환
-        //Todo: Shared
+    }
+
+    public void Do_Notification() {
+        //알람 설정
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_check_box_black_24dp)) //BitMap 이미지 요구
+                .setContentTitle("간호가노 상태창")
+                .setContentText("추가되었습니다")
+                // 더 많은 내용이라서 일부만 보여줘야 하는 경우 아래 주석을 제거하면 setContentText에 있는 문자열 대신 아래 문자열을 보여줌
+                //.setStyle(new NotificationCompat.BigTextStyle().bigText("더 많은 내용을 보여줘야 하는 경우..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT) //알림 바가 안뜨면 여기 수정해 보기
+                .setAutoCancel(true);
+
+        //OREO API 26 이상에서는 채널 필요
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create the NotificationChannel, but only on API 26+ because
+            // the NotificationChannel class is new and not in the support library
+            builder.setSmallIcon(R.drawable.ic_launcher_foreground); //mipmap 사용시 Oreo 이상에서 시스템 UI 에러남
+            CharSequence channelName = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+
+            NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, importance);
+            channel.setDescription(description);
+
+            // 노티피케이션 채널을 시스템에 등록
+            assert notificationManager != null;
+            //notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+        } else
+            builder.setSmallIcon(R.mipmap.ic_launcher); // Oreo 이하에서 mipmap 사용하지 않으면 Couldn't create icon: StatusBarIcon 에러남
+
+        assert notificationManager != null;
+        notificationManager.notify(1234, builder.build()); // 고유숫자로 노티피케이션 동작시킴
+
     }
 }
+
