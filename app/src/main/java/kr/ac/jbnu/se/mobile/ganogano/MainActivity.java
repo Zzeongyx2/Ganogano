f87bda8a5788c08245d58f7e925edcb0141e2704
jooyena <jooyena03@gmail.com>
2020-06-07 02:44:14 +0900

    add broadcastreceiver

--- app/src/main/java/kr/ac/jbnu/se/mobile/ganogano/MainActivity.java
+++ app/src/main/java/kr/ac/jbnu/se/mobile/ganogano/MainActivity.java
@@ -2,13 +2,17 @@ package kr.ac.jbnu.se.mobile.ganogano;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -21,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private int soundID;
     private int vol;//볼륨, 추후 수정
     TextView DDay;
+    //리시버 브로그캐스트 용
+    BroadcastReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
         btnMemo = (Button) findViewById(R.id.btn_memo);
         btnNote = (Button) findViewById(R.id.btn_note);
         btnSetting = findViewById(R.id.btn_setting);
-
 
         btnNote.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,5 +82,30 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if(intent.getAction().equals(Intent.ACTION_POWER_CONNECTED)){
+                    Toast.makeText(context, "충전기 연결!", Toast.LENGTH_LONG).show();
+                }else if(intent.getAction().equals(Intent.ACTION_BATTERY_LOW)){
+                    Toast.makeText(context, "배터리 부족!", Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        filter.addAction(Intent.ACTION_BATTERY_LOW);
+        registerReceiver(receiver,filter);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
     }
 }
